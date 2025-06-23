46bef4705df664e888698c34f7c8ceb44aaf941f
Dydhzo <dydhzo@proton.me>
2025-06-24 00:24:06 +0200

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -7,13 +7,17 @@ var config = {
 
 switch (env) {
     case 'local':
-        config.port = 64395;
+        config.port = process.env.PORT || 64395;
         config.local = "http://127.0.0.1:" + config.port;
         console.log(`Local mode activated. Configuration URL: ${config.local}, Port: ${config.port}`);
         break;
     case 'production':
         config.port = process.env.PORT || 64395;
         config.local = process.env.CONFIG_URL;
+        if (!config.local) {
+            console.error("FATAL ERROR: The CONFIG_URL environment variable is not set in production mode. The addon will not work without it.");
+            process.exit(1);
+        }
         console.log(`Production mode activated. Configuration URL: ${config.local}, Port: ${config.port}`);
         break;
 }
